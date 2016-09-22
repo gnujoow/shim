a160a79112133c8e5508427ab81332c76115a947
woojung <gnujoow.kim@gmail.com>
2016-09-22 18:07:41 +0900

    styling
    - sticky footer

--- src/containers/AppIndex.jsx
+++ src/containers/AppIndex.jsx
@@ -5,7 +5,7 @@ import Clock from '../components/Clock';
 export default class AppIndex extends React.Component {
   render() {
     return (
-      <div>
+      <div className="appIndex">
         <Clock />
         <h1>weather</h1>
         <h1>Quote</h1>
