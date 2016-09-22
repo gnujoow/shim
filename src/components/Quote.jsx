f8f3d741d8c95e1ea3690879058360e9515f2c9b
woojung <gnujoow.kim@gmail.com>
2016-09-22 19:13:25 +0900

    quote component
    mockup quote.jsx on appindex

---
+++ src/components/Quote.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Quote() {
+  return (
+    <h1>this is quote</h1>
+  );
+}
--- src/containers/AppIndex.jsx
+++ src/containers/AppIndex.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 
 import Clock from '../components/Clock';
+import Quote from '../components/Quote';
 
 export default function AppIndex() {
   return (
     <div className="appIndex">
       <div className="box">
         <Clock />
+        <Quote />
         <h1>weather</h1>
-        <h1>Quote</h1>
       </div>
     </div>
   );
