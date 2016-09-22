73578dc0291a4c9155d3615670ba579de0ae0c8a
woojung <gnujoow.kim@gmail.com>
2016-09-22 18:22:55 +0900

    AppIndex() lint
    class to function render

--- src/containers/AppIndex.jsx
+++ src/containers/AppIndex.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 
 import Clock from '../components/Clock';
 
-export default class AppIndex extends React.Component {
-  render() {
-    return (
-      <div className="appIndex">
-        <Clock />
-        <h1>weather</h1>
-        <h1>Quote</h1>
-      </div>
-    );
-  }
+export default function AppIndex() {
+  return (
+    <div className="appIndex">
+      <Clock />
+      <h1>weather</h1>
+      <h1>Quote</h1>
+    </div>
+  );
 }
